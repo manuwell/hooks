f6730b42107343194b4c243c63dfd42b9f30ffb8
manuwell <wjsantos.wj@gmail.com>
2017-05-04 17:24:59 -0300

    Persists requests on redis

--- web/controllers/api/hook_handler_controller.ex
+++ web/controllers/api/hook_handler_controller.ex
@@ -1,14 +1,33 @@
 defmodule Hooks.Api.HookHandlerController do
   use Hooks.Web, :controller
+  alias Exredis.Api, as: Redis
 
-  def handler(conn, _params) do
+  def handler(conn, %{"id" => id}) do
     headers = for e <- conn.req_headers, into: %{}, do: e
     raw_body = conn.private[:raw_body]
+
+    key = [id, "request", request_id] |> Enum.join(":")
+    requests_key = [id, "requests"] |> Enum.join(":")
 
     response = %{
       headers: headers,
       raw_body: raw_body
     }
-    json conn, response
+
+    Redis.append(:redis,
+      requests_key,
+      [key, "|"] |> Enum.join
+    )
+    content = response |> Poison.encode!
+    Redis.set(:redis, key, content)
+
+    conn
+    |> send_resp(200, "")
+  end
+
+  defp request_id do
+    UUID.uuid4()
+    |> String.split("-")
+    |> List.first
   end
 end
